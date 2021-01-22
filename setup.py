ae6d78f85d0ea0ae39b588e2c71294688f00fda3
bitjoy <chenzhenlin@ict.ac.cn>
2021-01-22 16:36:09 +0800

    try pypi and initially commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pDeepXL",
+    version="1.0.1",
+    author="Zhenlin Chen",
+    author_email="chenzhenlin@ict.ac.cn",
+    description="MS/MS spectrum prediction for cross-linked peptide pairs by deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pFindStudio/pDeepXL",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
